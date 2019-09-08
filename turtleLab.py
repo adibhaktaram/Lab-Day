a192c6a22cc47c2d60804fdf45096a4f46aaa19b
Adi Bhaktaram <adithyabhaktaram@casady.org>
2019-09-08 12:12:45 -0500

    completed turtle lab

--- turtleLab.py
+++ turtleLab.py
@@ -14,16 +14,34 @@ def polygon(t, length, n):
 
 bob = turtle.Turtle()
 
-polygon(bob, 1, 360)
+def circle(t, r,):
+    circumference = 2 * math.pi *r
+    n = int(circumference / 3) + 1
+    length = circumference / n
+    polygon(bob, length, n)
 
-def circle(t, r):
-    r = (length*n)/(2*math.pi)
-    n = (2*math.pi*r)/length
+bob = turtle.Turtle()
+bob.ht()
 
-    polygon(t, length, n)
+def polygonTwo(t, length, n):
+        for _ in range(n * angle):
+            angle = angle/360
+            t.fd(length)
+            t.lt(360 / n)
 
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = angle / n
 
+    for _ in range(n):
+        t.fd(step_length)
+        t.lt(step_angle)
 
+bob = turtle.Turtle()
+bob.ht()
 
+arc(bob, 100, 180)
 
 turtle.mainloop()
