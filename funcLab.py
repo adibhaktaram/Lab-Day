c02f1f7e255451bebb522f56c4d1d2173bc8e41b
Adi Bhaktaram <adithyabhaktaram@casady.org>
2019-09-13 23:23:20 -0500

    partially completed

--- funcLab.py
+++ funcLab.py
@@ -1,15 +1,31 @@
 def arg(x):
-    print(x)
+    '''
+    Description: This function prints the type of the argument passed.
 
-arg(2)
+    Parameters: x - variable; what the user enters in the argument
+
+    Example:
+    >>> arg(5)
+    '''
+    print(type(x))
+
 
 def no_ret(x,y):
     var = x + y
 
 print(no_ret(2,4))
-#the output is none because you never tell it to print var
+'''
+The output is none because the function is called wrong and print shoudl be in
+the function definition
+'''
 
 def draw():
+    '''
+    Description: This function draws a grid represented on page 33 of the book
+    in exercise 3-3.
+
+    Parameters: None
+    '''
     print('+', '- '*4, '+','- '*4, '+')
     print('|', ' '*8, '|', ' '*8, '|')
     print('|', ' '*8, '|', ' '*8, '|')
@@ -21,9 +37,15 @@ def draw():
     print('|', ' '*8, '|', ' '*8, '|')
     print('|', ' '*8, '|', ' '*8, '|')
     print('+', '- '*4, '+','- '*4, '+')
-draw()
+
 
 def draw2():
+    '''
+    Description: This function draws a grid similar to the one in the function
+    draw() but it has 4 rows and columns.
+
+    Parameters: None
+    '''
     print('+', '- '*4, '+','- '*4, '+', '- '*4, '+','- '*4, '+')
     print('|', ' '*8, '|', ' '*8, '|', ' '*8, '|', ' '*8, '|')
     print('|', ' '*8, '|', ' '*8, '|', ' '*8, '|', ' '*8, '|')
@@ -45,16 +67,41 @@ def draw2():
     print('|', ' '*8, '|', ' '*8, '|', ' '*8, '|', ' '*8, '|')
     print('|', ' '*8, '|', ' '*8, '|', ' '*8, '|', ' '*8, '|')
     print('+', '- '*4, '+','- '*4, '+', '- '*4, '+','- '*4, '+')
-draw2()
+
 
 def nameAge(x, y):
+    '''
+    Description: This function prints the name of someone and how old they are
+    days
+
+    Parameters: x - name of the user's choice
+                y - age of the name of the user's choice
+
+    Example:
+    >>> nameAge(Adi, 14)
+    '''
     n = y*(365.25)
     print( x + ' is ' + str(n) + ' days old' )
-nameAge('Adi', 14)
+
 
 def lenList(myList):
+    '''
+    Description: This function prints the length of a list that is passed in
+    the argument
+
+    Parameters: myList - The list that the user passes
+    Example:
+    >>> thisList=('apples, oranges, kiwis')
+    >>> lenList(thisList)
+
+    '''
     length = len(myList)
     print(length)
 
-thisList=list('apples, oranges, kiwis')
-lenList(thisList)
+def squareCube(n):
+    x = n**2
+    y = n**3
+    print('Number', '    ', 'Square', '    ', 'Cube')
+    print(' '*(5 - len(str(n))), n, '    ' ' '*(6 - len(str(x))), x, '    ', ' '*(4 - len(str(y))), y)
+
+squareCube(4)
--- guessingGame.py
+++ guessingGame.py
@@ -1,20 +1,19 @@
 import random
 def guessingGame():
     rndNmbr = random.randint(0,20)
-    numGuesses = 10
+    numGuesses = 5
     counter = 0
     while(counter < numGuesses):
         guess = input("Guess a number:")
-        if (int(guess == rndNmbr)):
+        if (int(guess) == rndNmbr):
             print("You Win!")
             break
         if (counter < numGuesses - 1):
-            print("Try Again")
+            if (int(guess) > rndNmbr):
+                    print("Lower")
+            if (int(guess) < rndNmbr):
+                    print("Higher")
             counter += 1
-            if int(guess < rndNmbr):
-                print("Lower")
-            if int(guess < rndNmbr):
-                print("Higher")
         else:
             print("You Lost!")
             break
